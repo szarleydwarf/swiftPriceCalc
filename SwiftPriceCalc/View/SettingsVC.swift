b4f3ed761bff1477887404e3f3a4a60f43af88f3
RJ <r.j.chomik@gmail.com>
2021-01-05 14:15:33 +0000

    VC supporting files for other views

--- SwiftPriceCalc/View/SettingsVC.swift
+++ SwiftPriceCalc/View/SettingsVC.swift
@@ -1,19 +1,15 @@
 //
-//  ViewController.swift
+//  SettingsVC.swift
 //  SwiftPriceCalc
 //
-//  Created by The App Experts on 04/01/2021.
+//  Created by The App Experts on 05/01/2021.
 //  Copyright © 2021 The App Experts. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
+class SettingsVC: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
 }
-
